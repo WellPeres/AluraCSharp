222717598ddd04b3395182016d93c0fdd82cbdb1
Wellinton Batista de Oliveira <wellinton.oliveira@SULBRASIL.LOCAL>
2022-10-03 14:13:22 -0300

    first commit

---
+++ 3-CriandoVariaveisTipoFloat/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Pontos Flutuante");
+
+        double salario;
+        salario = 3000.10;
+
+        Console.WriteLine("O meu salário é: " + salario);
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
---
+++ 4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        double salario = 3000.15;
+        Console.WriteLine(salario);
+
+        int salarioInteiro = (int)salario;
+        Console.WriteLine(salarioInteiro);
+         
+        //O long é um tipo de variável de 64 bits
+        long x = 2000000000000000000;
+        Console.WriteLine(x);
+
+        //O short é um tipo de variável de 32 bits
+        short y = 15000;
+        Console.WriteLine(y);
+       
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
